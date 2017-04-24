ec216265f754e6e69e051c573aef8cc326089bbb
Tina Pistorius <tina.pistorius@netronome.com>
2017-04-24 15:16:19 +0200

    app config: add stats error cnt for MTU

---
+++ firmware/lib/nic_basic/nic_stats.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Netronome, Inc.
+ *
+ * @file          lib/nic/_c/nic_stats.c
+ * @brief         Implementation for additional stats
+ */
+
+#ifndef _LIBNIC_NIC_STATS_H_
+#define _LIBNIC_NIC_STATS_H_
+
+/*
+ * Additional counters for the NIC application
+ *
+ * Most of the statistics for the NIC are directly based on stats
+ * maintained by the MAC.  However, some required stats are either
+ * derived counts or software based counts.  This structure defines
+ * these additional stats.
+ *
+ * DO NOT CHANGE THE ORDER!
+ */
+struct nic_port_stats_extra {
+    unsigned long long rx_discards;
+    unsigned long long rx_errors;
+    unsigned long long rx_uc_octets;
+    unsigned long long rx_mc_octets;
+    unsigned long long rx_bc_octets;
+    unsigned long long rx_uc_pkts;
+    unsigned long long rx_mc_pkts;
+    unsigned long long rx_bc_pkts;
+
+    unsigned long long tx_discards;
+    unsigned long long tx_errors;
+    unsigned long long tx_uc_octets;
+    unsigned long long tx_mc_octets;
+    unsigned long long tx_bc_octets;
+    unsigned long long tx_uc_pkts;
+    unsigned long long tx_mc_pkts;
+    unsigned long long tx_bc_pkts;
+};
+
+#endif
