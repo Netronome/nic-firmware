5f345e03a7cd7fdcf3891fbf46548839e55d0585
Rami Tomer <rami.tomer@netronome.com>
2017-06-12 14:46:34 +0300

    Swap tmq drop counters endianness
    wip://NIC-225
    
    Signed-off-by: Rami Tomer <rami.tomer@netronome.com>
    Acked-by: Pablo Cascón <pablo.cascon@netronome.com>

--- firmware/lib/nic_basic/_c/nic_stats.c
+++ firmware/lib/nic_basic/_c/nic_stats.c
@@ -17,6 +17,7 @@
 #include <vnic/shared/nfd_cfg.h>
 #include <vnic/pci_out.h>
 
+extern __intrinsic uint64_t swapw64(uint64_t val);
 
 /*
  * Additional counters for the NIC application
@@ -289,7 +290,7 @@ nic_stats_tx_counters(int port, __xwrite struct cfg_bar_cntrs *write_bar_cntrs)
 
         /* Accumulate the TM Q drops */
         mem_read64(&read_val, &nic_tmq_drop_cntr_accum[port], sizeof(uint64_t));
-        bar_cntrs.discards += read_val;
+        bar_cntrs.discards += swapw64(read_val);
     }
 #endif
 
