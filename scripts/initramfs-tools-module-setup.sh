779ebd903ea41d1ecbc3aa937bc3f3cc55a9798c
Pablo Cascón <pablo.cascon@netronome.com>
2017-08-03 13:19:44 +0100

    build: improve initramfs hook to get the actual FW file, not symlink
    On some machines the initramfs was not generated correctly, only the
    symlinks will appear. That will make the nfp driver not find the FW.
    This has been tested manually making sure after a reboot the driver
    was loading the FW.
    
    Signed-off-by: Pablo Cascón <pablo.cascon@netronome.com>

--- scripts/initramfs-tools-module-setup.sh
+++ scripts/initramfs-tools-module-setup.sh
@@ -17,14 +17,13 @@ esac
 # nfp dependency must be available, else quit without error
 have_module nfp || exit 0
 
-# Add NFP kernel module
-manual_add_modules nfp
-
-
 # Add firmware images
 for FILE in `find /lib/firmware/netronome -maxdepth 1 -name *.nffw`; do
     TARGET=`readlink $FILE`
     copy_exec $TARGET /lib/firmware/netronome/
 done
+
+# Add NFP kernel module
+manual_add_modules nfp
 
 exit 0
