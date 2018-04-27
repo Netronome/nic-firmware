eb6eceb7e624fa4f78c75423fec7b94c3691da0d
Norm Bagley <norman.bagley@netronome.com>
2018-04-27 15:12:26 -0400

    [tests] add framework for microc
    This patch enhances the unit test runner libraries to enable testing
    of MicroC functions.
    
    resolves://NIC-544
    
    Signed-off-by: Norm Bagley <norman.bagley@netronome.com>
    Reviewed-by: Edwin Peer <edwin.peer@netronome.com>

---
+++ test/lib/test.c
@@ -0,0 +1,55 @@
+/*
+    test support functions
+*/
+
+#include <rtl.c>
+
+/* stop all threads except thread 0 */
+__intrinsic void single_ctx_test() {
+    uint32_t sts = local_csr_read(local_csr_active_ctx_sts);
+    if (sts & 7 )
+        __asm ctx_arb[kill];
+}
+
+/* test passed */
+__intrinsic void test_pass() {
+    local_csr_write(local_csr_mailbox0, 0x01);
+}
+
+/* test failed with fail code */
+__intrinsic _test_fail(uint32_t fail_type) {
+    uint32_t sts = local_csr_read(local_csr_active_ctx_sts);
+    local_csr_write(local_csr_mailbox0, fail_type);
+    local_csr_write(local_csr_mailbox1, sts);
+    __asm ctx_arb[kill];
+}
+
+/* test failed, code set to 0xff */
+__intrinsic void test_fail() {
+    _test_fail(0xff);
+}
+
+/* assert (fail test zero value) */
+__intrinsic void test_assert(uint32_t _assert) {
+    if (!_assert)
+        _test_fail(0xfa);
+}
+
+/* assert when values are not equal */
+__intrinsic void test_assert_equal(uint32_t tested,
+                                   uint32_t expected) {
+    if ( tested != expected ) {
+        local_csr_write(local_csr_mailbox2, tested);
+        local_csr_write(local_csr_mailbox3, expected);
+        _test_fail(0xfc);
+    }
+}
+
+/* assert when values are equal */
+__intrinsic void test_assert_unequal(uint32_t tested,
+                                     uint32_t expected) {
+    if ( tested == expected ) {
+        local_csr_write(local_csr_mailbox2, tested);
+        _test_fail(0xfe);
+    }
+}
