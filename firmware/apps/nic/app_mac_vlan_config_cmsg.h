d3215f80c78a8882cc54cccbe360346936201e3c
Norm Bagley <norman.bagley@netronome.com>
2018-05-31 10:46:04 -0400

    [config] add VEB table entry control message marshaller
    The VEB table reuses the BPF map code, which does not presently
    have a MicroC implementation. BPF map entries are manipulated
    via an auxiliary ME written in microcode that handles control
    messages from the host.
    
    For SR-IOV VEB entries, a control message to this auxiliary ME
    is generated in order to add and remove table entries in response
    to control BAR updates in the app_master MicroC context. This
    patch comprises the plumbing needed to marshal these messages
    to the control message handler ME.
    
    wip://NIC-500
    resolves://NIC-516
    
    Signed-off-by: Norm Bagley <norman.bagley@netronome.com>
    Reviewed-by: Edwin Peer <edwin.peer@netronome.com>

---
+++ firmware/apps/nic/app_mac_vlan_config_cmsg.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2016-2017 Netronome, Inc.
+ *
+ * @file          apps/nic/app_mac_vlan_config_cmsg.h
+ * @brief         NIC application MAC+VLAN lookup cmsg table config
+ */
+
+#ifndef _APP_MAC_VLAN_CONFIG_CMSG_H_
+#define _APP_MAC_VLAN_CONFIG_CMSG_H_
+
+/* *** NIC MAC+VLAN Table Configuration Settings. *** */
+
+#define NIC_MAC_VLAN_TABLE__NUM_ENTRIES 0x4000
+
+/* *** NIC MAC+VLAN table result types. *** */
+#define NIC_MAC_VLAN_RES_TYPE_TO_HOST 0
+#define NIC_MAC_VLAN_RES_TYPE_TO_WIRE 1
+#define NIC_MAC_VLAN_RES_TYPE_LACP    2
+
+/* *** NIC MAC+VLAN Data Structures. *** */
+
+#define NIC_MAC_VLAN_KEY_SIZE_LW   2
+
+#define MAP_CMSG_IN_WQ_SZ	4096
+
+#define SRIOV_QUEUE  64
+
+#define CMESG_DISPATCH_OK       1
+#define CMESG_DISPATCH_FAIL     0xffffffff
+
+/** Lookup key for the MAC+VLAN table. */
+#if defined(__NFP_LANG_MICROC)
+    struct nic_mac_vlan_key {
+        union {
+            struct {
+                unsigned int __unused : 3;
+                unsigned int s_not_c  : 1;  /**< VLAN tag is S-VLAN */
+                unsigned int vlan_id  : 12; /**< VLAN ID */
+                uint16_t mac_addr_hi;       /**< Upper 2 bytes of MAC address */
+                uint32_t mac_addr_lo;       /**< Lower 4 bytes of MAC address */
+            };
+            uint32_t __raw[NIC_MAC_VLAN_KEY_SIZE_LW];
+        };
+    };
+
+    #define NIC_MAC_VLAN_RESULT_SIZE_LW 16
+    uint32_t action_list[(NIC_MAC_VLAN_RESULT_SIZE_LW)];
+
+    #define NIC_MAC_VLAN_ENTRY_SIZE_LW \
+        (NIC_MAC_VLAN_KEY_SIZE_LW + NIC_MAC_VLAN_RESULT_SIZE_LW)
+
+    /** Lookup entry for MAC+VLAN table. */
+    struct nic_mac_vlan_entry {
+        union {
+            struct {
+                struct nic_mac_vlan_key key;
+                uint32_t action_list[(NIC_MAC_VLAN_RESULT_SIZE_LW)];
+            };
+            uint32_t __raw[NIC_MAC_VLAN_ENTRY_SIZE_LW];
+        };
+    };
+
+    #define NIC_MAC_VLAN_CMSG_SIZE_LW  \
+        (NIC_MAC_VLAN_KEY_SIZE_LW + 4) //check this
+
+    /** Cmsg st MAC+VLAN Structure for table. */
+    struct nic_mac_vlan_cmsg {
+        union {
+            struct {
+                uint32_t word0;  /* CMSG_TYPE_MAP_xxx add, delete, lookup, getnext, getfirst */
+                uint32_t tid;
+                uint32_t count;
+                uint32_t flags;
+                struct nic_mac_vlan_key key;
+            };
+            uint32_t __raw[NIC_MAC_VLAN_CMSG_SIZE_LW];
+        };
+    };
+
+    /* *** NIC MAC+VLAN Table Functions. *** */
+
+    /**
+    * Add a MAC+VLAN entry to the NIC MAC+VLAN table.
+    *
+    * @param key           Key for the entry to add
+    * @param action_list   Address of the SRIOV action list
+    * @param operation     Add/Del Entry
+    */
+    int32_t nic_mac_vlan_entry_op_cmsg(__lmem struct nic_mac_vlan_key *key,
+                                       __lmem uint32_t *action_list,
+                                       const int operation);
+#endif
+#endif /* ndef _APP_MAC_VLAN_CONFIG_CMSG_H_ */
