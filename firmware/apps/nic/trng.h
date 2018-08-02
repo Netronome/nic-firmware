16163e11c402402e532f978832d4d9d4c7fe7c69
Edwin Peer <edwin.peer@netronome.com>
2018-08-02 01:43:51 -0700

    [app_master] initialize VF MACs using TRNG
    Usually VF MAC addresses are configured by the host, however,
    in some configurations the host relies on the NIC to provide a
    suitable address. Since we do not have BSP MAC allocations for
    VFs these must be randomly generated. Use the hardware's true
    random capability to avoid MAC collisions for hardware that
    is brought up in similar starting conditions (two hosts being
    powered up at the same time, for example).
    
    Signed-off-by: Ciaran Toal <ciaran.toal@netronome.com>
    Reviwed-by: Edwin Peer <edwin.peer@netronome.com>

---
+++ firmware/apps/nic/trng.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2015 Netronome, Inc. All rights reserved.
+ *
+ * @file          apps/nic/trng.h
+ * @brief         Header file for trng.c
+ */
+ #ifndef __TRNG_H__
+ #define __TRNG_H__
+
+ // TRNG defines
+
+ // addresses
+ #define TRNG_ASYNC_RING           0x00
+ #define TRNG_ASYNC_TEST           0x04
+ #define TRNG_ASYNC_CMD            0x08
+ #define TRNG_ASYNC_STATUS         0x0C
+ #define TRNG_ASYNC_CFG            0x10
+ #define TRNG_LFSR_CFG             0x20
+ #define TRNG_WHITEN_CONTROL       0x24
+ #define TRNG_WHITEN_CONFIG        0x28
+ #define TRNG_MON_PERIOD           0x30
+ #define TRNG_MON_ONES             0x34
+ #define TRNG_MON_ONES_MIN         0x38
+ #define TRNG_MON_ONES_MAX         0x3c
+ #define TRNG_MON_MAX_RUN_LEN      0x40
+ #define TRNG_LOCK                 0x50
+ #define TRNG_ALERT                0x54
+
+ #define CLS_TRNG_XPB_DEVICE_ID      12
+ #define TH_TRNG_data                 0
+
+ #define CLS_PERIPHERAL_TRNG_DATA      0x60000
+ #define CLS_PERIPHERAL_TRNG_DATA_ALT  0x60001
+
+__intrinsic void trng_init();
+
+__intrinsic void trng_init_add_delay(const int delay_count);
+
+__intrinsic void trng_rd64(uint32_t *trng_hi, uint32_t *trng_lo);
+
+#endif // __TRNG_H__
