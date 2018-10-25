654453f50cb43048fbc982fe5341fa61bc32410c
Johan Moraal <johan.moraal@netronome.com>
2018-10-25 10:22:19 +0200

    test: process_pf_reconfig: Check invalid control and update word
    This test checks that the function rejects invalid control and update words
    
    Signed-off-by: Johan Moraal <johan.moraal@netronome.com>

---
+++ test/nfd_app_master/app_master_process_pf_reconfig_invalid_test.c
@@ -0,0 +1,65 @@
+/*
+    Tests the process_pf_reconfig funtion in app_master
+*/
+
+#include "defines.h"
+#include "test.c"
+#include "app_master_test.h"
+#include "vnic_setup.c"
+#include "app_private.c"
+#include "app_config_tables.c"
+#include "nic_tables.c"
+#include "app_mac_vlan_config_cmsg.c"
+#include "app_control_lib.c"
+#include "nfd_cfg_base_decl.c"
+
+void test(uint32_t pcie) {
+    uint32_t type, vnic, vid, pf, control, update;
+    struct nfd_cfg_msg cfg_msg;
+
+    for (pf = 0; pf < NFD_MAX_PFS; pf++) {
+
+        vid = NFD_PF2VID(pf);
+        NFD_VID2VNIC(type, vnic, vid);
+
+        reset_cfg_msg(&cfg_msg, vid, 0);
+
+        //Invalid control, valid update
+        control = ~NFD_CFG_PF_CAP;
+        update = NFD_CFG_PF_LEGAL_UPD & ~NFP_NET_CFG_UPDATE_BPF; //BPF updates tested separately
+        if (process_pf_reconfig(control, update, vid, vnic, &cfg_msg)) {
+             if(cfg_msg.error == 0)
+                 test_fail();
+        } else {
+            test_fail();
+        }
+
+        //Valid control, invalid update
+        control = NFD_CFG_PF_CAP;
+        update = ~NFD_CFG_PF_LEGAL_UPD;
+        if (process_pf_reconfig(control, update, vid, vnic, &cfg_msg)) {
+             if(cfg_msg.error == 0)
+                 test_fail();
+        } else {
+            test_fail();
+        }
+
+        //invalid control, invalid update
+        control = ~NFD_CFG_PF_CAP;
+        update = ~NFD_CFG_PF_LEGAL_UPD;
+        if (process_pf_reconfig(control, update, vid, vnic, &cfg_msg)) {
+             if(cfg_msg.error == 0)
+                 test_fail();
+        } else {
+            test_fail();
+        }
+    }
+
+    test_pass();
+}
+
+void main(void)
+{
+    single_ctx_test();
+    test(0);
+}
