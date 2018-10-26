5c10f987a5724c6d59ac949599ba18a70b425397
Johan Moraal <johan.moraal@netronome.com>
2018-10-26 09:54:52 +0200

    test: process_pf_reconfig: Test process_pf_reconfig when VF's are up
    Checks the return value of process_pf_reconfig when the VF's are up.
    
    Signed-off-by: Johan Moraal <johan.moraal@netronome.com>

---
+++ test/nfd_app_master/app_master_process_pf_reconfig_vf_enable_test.c
@@ -0,0 +1,67 @@
+//TEST_REQ_BLM
+//TEST_REQ_RESET
+
+/*
+    Tests the process_pf_reconfig funtion in app_master
+*/
+
+#include "defines.h"
+#include "test.c"
+#include "app_master_test.h"
+#include "vnic_setup.c"
+#include "app_private.c"
+#include "app_config_tables.c"
+#include "nic_tables.c"
+#include "map_cmsg_rx.c"
+#include "app_control_lib.c"
+#include "nfd_cfg_base_decl.c"
+
+void test(uint32_t pcie) {
+    uint32_t type, vnic, vid, pf, control, update;
+    int vf;
+    uint32_t test_control;
+    struct nfd_cfg_msg cfg_msg;
+
+    //First indicate VF's are enabled
+    for (vf = 0; vf < NFD_MAX_VFS; vf++) {
+
+        vid = NFD_VF2VID(vf);
+        NFD_VID2VNIC(type, vnic, vid);
+
+        set_nic_control_word(NIC_PCI, vid, get_nic_control_word(NIC_PCI, vid) | NFP_NET_CFG_CTRL_ENABLE);
+        setup_sriov_cfg_data(NIC_PCI, vf, TEST_MAC, 0, NFD_VF_CFG_CTRL_LINK_STATE_ENABLE);
+
+    }
+
+    for (pf = 0; pf < NFD_MAX_PFS; pf++) {
+
+        vid = NFD_PF2VID(pf);
+        NFD_VID2VNIC(type, vnic, vid);
+
+        reset_cfg_msg(&cfg_msg, vid, 0);
+
+        setup_pf_mac(NIC_PCI, vid, TEST_MAC);
+
+        control = NFD_CFG_PF_CAP;
+        update = NFD_CFG_PF_LEGAL_UPD & ~NFP_NET_CFG_UPDATE_BPF; //BPF updates tested separately
+
+        if (process_pf_reconfig(control, update, vid, vnic, &cfg_msg)) {
+            test_fail();
+        }
+    }
+
+    test_pass();
+}
+
+void main(void)
+{
+    switch (ctx()) {
+        case 0:
+            test(0);
+            break;
+        default:
+            map_cmsg_rx();
+            break;
+    }
+}
+
