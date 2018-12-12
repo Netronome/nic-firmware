ec134802698fd2f16a9df589f679d9c670b7b968
Johan Moraal <johan.moraal@netronome.com>
2018-12-12 08:10:09 +0200

    test: Add NFCC -v1 flag to Micro C unit tests.
    Signed-off-by: Johan Moraal <johan.moraal@netronome.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -31,7 +31,7 @@ for t in `find ${TEST_DIR} -iname '*_test.uc' -o -iname '*_test.c'` ; do
         for obj in `find ${FW_BUILD_DIR} -iname '*.obj'` ; do
             of=$of" "$obj
         done
-        nfcc -chip nfp-4xxx-b0 -Itest/include -Itest/lib $* -o ${TEST_BUILD_DIR}/${FILE_BASE}.list $t ${of} || exit 1
+        nfcc -chip nfp-4xxx-b0 -v1 -Itest/include -Itest/lib $* -o ${TEST_BUILD_DIR}/${FILE_BASE}.list $t ${of} || exit 1
         nfld -chip nfp-4xxx-b0 -mip -rtsyms -map -u i32.me0 ${TEST_BUILD_DIR}/${FILE_BASE}.list || exit 1
     fi
 
