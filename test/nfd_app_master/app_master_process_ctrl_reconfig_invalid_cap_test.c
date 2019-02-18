6a8e55f625cce45d256d4e27b7db2876923750f9
Johan Moraal <johan.moraal@netronome.com>
2019-02-18 09:57:37 +0200

    test: add invalid case for process_ctrl_reconfig()
    Check return code when invalid control word is passed.
    
    Signed-off-by: Johan Moraal <johan.moraal@netronome.com>
    Reviewed-by: Edwin Peer <edwin.peer@netronome.com>

---
+++ test/nfd_app_master/app_master_process_ctrl_reconfig_invalid_cap_test.c
@@ -0,0 +1,37 @@
+/*
+   Tests the process_ctrl_reconfig funtion in app_master
+   */
+
+#include "defines.h"
+#include "test.c"
+#include "vnic_setup.c"
+#include "app_private.c"
+#include "app_control_lib.c"
+#include "app_config_tables.c"
+#include "nfd_cfg_base_decl.c"
+
+void main() {
+    uint32_t control, vid, ctrl;
+    struct nfd_cfg_msg cfg_msg;
+
+    single_ctx_test();
+    for (ctrl = 0; ctrl < NFD_MAX_CTRL; ctrl++) {
+
+        vid = NFD_CTRL2VID(ctrl);
+
+        //test invalid control CAP
+        reset_cfg_msg(&cfg_msg, vid, 0);
+
+        control = ~NFD_CFG_CTRL_CAP;
+
+        if(process_ctrl_reconfig(control, vid, &cfg_msg)) {
+            if(cfg_msg.error == 0)
+                test_fail();
+        } else {
+            test_fail();
+        }
+
+    }
+
+    test_pass();
+}
