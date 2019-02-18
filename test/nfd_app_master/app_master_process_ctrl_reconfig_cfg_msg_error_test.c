f8f1d925bdf879d90e7dc6eeda76703732c7203a
Johan Moraal <johan.moraal@netronome.com>
2019-02-18 09:57:34 +0200

    test: add error case for process_ctrl_reconfig()
    Check link state when passed error cfg message.
    
    Signed-off-by: Johan Moraal <johan.moraal@netronome.com>
    Reviewed-by: Edwin Peer <edwin.peer@netronome.com>

---
+++ test/nfd_app_master/app_master_process_ctrl_reconfig_cfg_msg_error_test.c
@@ -0,0 +1,36 @@
+/*
+   Tests the process_ctrl_reconfig funtion in app_master
+   */
+
+#include "defines.h"
+#include "test.c"
+#include "vnic_setup.c"
+#include "app_private.c"
+#include "app_control_lib.c"
+#include "app_config_tables.c"
+#include "nfd_cfg_base_decl.c"
+
+void main() {
+    uint32_t control, vid, ctrl;
+    struct nfd_cfg_msg cfg_msg;
+    __xread unsigned int link_state;
+
+    single_ctx_test();
+    for (ctrl = 0; ctrl < NFD_MAX_CTRL; ctrl++) {
+
+        vid = NFD_CTRL2VID(ctrl);
+
+        //test link status: NO LINK (cfg_msg.error = 1)
+        reset_cfg_msg(&cfg_msg, vid, 1);
+
+        control = NFD_CFG_CTRL_CAP & ~NFP_NET_CFG_CTRL_ENABLE;
+
+        if(process_ctrl_reconfig(control, vid, &cfg_msg))
+            test_fail();
+
+        mem_read32(&link_state, (NFD_CFG_BAR_ISL(PCIE_ISL, vid) + NFP_NET_CFG_STS), sizeof link_state);
+        test_assert_equal(link_state, 0);
+    }
+
+    test_pass();
+}
