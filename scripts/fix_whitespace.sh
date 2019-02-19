c6e4c1499b6db62423680ab3705d971559641bd7
Johan Moraal <johan.moraal@netronome.com>
2019-02-19 15:45:08 +0200

    scripts: substitute spaces for tabs in fix_whitespace.sh
    Add rules conforming to CodingStyle.md. Tabs should be replaced with 4 spaces.
    
    wip://NIC-678
    
    Signed-off-by: Johan Moraal <johan.moraal@netronome.com>
    Reviewed-by: Edwin Peer <edwin.peer@netronome.com>

--- scripts/fix_whitespace.sh
+++ scripts/fix_whitespace.sh
@@ -1,4 +1,7 @@
 find ./ -iname '*.h' -exec sed -i 's/[[:space:]]\+$//' {} \;
 find ./ -iname '*.c' -exec sed -i 's/[[:space:]]\+$//' {} \;
 find ./ -iname '*.uc' -exec sed -i 's/[[:space:]]\+$//' {} \;
+find ./ -iname '*.h' -exec sed -i 's/\t/    /g' {} \;
+find ./ -iname '*.c' -exec sed -i 's/\t/    /g' {} \;
+find ./ -iname '*.uc' -exec sed -i 's/\t/    /g' {} \;
 
