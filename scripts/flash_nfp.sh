1545833d18b554a0732ba0ab9b9fe07fd31d4a1c
Edwin Peer <edwin.peer@netronome.com>
2019-08-03 18:28:21 -0700

    scripts: remove obsolete scripts
    Signed-off-by: Edwin Peer <edwin.peer@netronome.com>
    Signed-off-by: Nick Viljoen <nick.viljoen@netronome.com>
    Reviewed-by: Johan Moraal <johan.moraal@netronome.com>
    Reviewed-by: Dirk van der Merwe <dirk.vandermerwe@netronome.com>

--- scripts/flash_nfp.sh
+++
@@ -1,5 +0,0 @@
-#ToDo - Skip nfp-one command for Starfighter/Starshuttle 
-
-/opt/netronome/bin/nfp-flash --i-accept-the-risk-of-overwriting-miniloader --preserve-media-overrides -w /opt/netronome/flash/flash-nic.bin
-yes | /opt/netronome/bin/nfp-one
-reboot
