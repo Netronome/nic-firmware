abaf91dee124c46f712e48c400a0b3fd41354d2b
Johan Moraal <johan.moraal@netronome.com>
2020-01-21 10:37:18 +0200

    nic_tables.c: Fix whitespace
    Change-Id: I7ad9209bb7e8e9d2493b13903bba5529f68f80bd
    Signed-off-by: Johan Moraal <johan.moraal@netronome.com>
    Reviewed-by: Michael Rapson <michael.rapson@netronome.com>
    Reviewed-on: https://ci.netronome.com/r/1584
    Tested-by: Zuul CI <zuul@ci.netronome.com>

--- firmware/apps/nic/nic_tables.c
+++ firmware/apps/nic/nic_tables.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2019 Netronome Systems, Inc. All rights reserved.
+ * Copyright (c) 2016-2020 Netronome Systems, Inc. All rights reserved.
  *
  * @file          apps/nic/nic_tables.c
  * @brief         allocate tables for Core NIC
@@ -41,7 +41,7 @@ add_vlan_member(uint16_t vlan_id, uint16_t vid)
     uint64_t min_rxb;
 
     if (vlan_id > NIC_MAX_VLAN_ID)
-	return -1;
+        return -1;
 
     mem_read64(&members_r, &nic_vlan_to_vnics_map_tbl[vlan_id], sizeof(uint64_t));
     min_rxb = (members_r >> 58);
@@ -76,14 +76,14 @@ remove_vlan_member(uint16_t vid)
         if (members) {
             min_rxb = (1 << 6) - 1;
             for (vid_idx = 0; NFD_MAX_VFS && vid_idx < NFD_MAX_VFS; vid_idx++) {
-	        if (members & (1ull << vid_idx)) {
+                if (members & (1ull << vid_idx)) {
                     mem_read32(&rxb_r,
-		               (__mem void*) (NFD_CFG_BAR_ISL(NIC_PCI, vid_idx) +
+                               (__mem void*) (NFD_CFG_BAR_ISL(NIC_PCI, vid_idx) +
                                NFP_NET_CFG_FLBUFSZ), sizeof(rxb_r));
                     min_rxb = (min_rxb < (rxb_r >> 8) & 0x3f) ? min_rxb : (rxb_r >> 8) & 0x3f;
                 }
             }
-	    members |= (min_rxb << 56);
+            members |= (min_rxb << 56);
         }
         members_w = members;
         mem_write64(&members_w, &nic_vlan_to_vnics_map_tbl[vlan], sizeof(uint64_t));
