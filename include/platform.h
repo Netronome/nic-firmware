9b884f85f52af6ff5a93fb69e0df6fcfab2fb13c
Xingfeng Hu <xingfeng.hu@corigine.com>
2021-07-13 18:48:11 +0800

    build: add support for 2x10G kestrel nic scarif AMDA0145
    Scarif is a 2x10G kestrel NIC, So, the configuration
    and function assignment is the same as Alderaan_2x10

--- include/platform.h
+++ include/platform.h
@@ -52,8 +52,9 @@
 #define NS_PLATFORM_CADMIUM_DDR_1x50            19
 #define NS_PLATFORM_ALDERAAN                    20
 #define NS_PLATFORM_ALDERAAN_2x10               21
+#define NS_PLATFORM_SCARIF                      22
 
-#define NS_PLATFORM_LAST                        NS_PLATFORM_ALDERAAN_2x10
+#define NS_PLATFORM_LAST                        NS_PLATFORM_SCARIF
 
 
 /*
@@ -685,6 +686,27 @@
 
 /* Alderaan 2x10GE */
 #elif (NS_PLATFORM_TYPE == NS_PLATFORM_ALDERAAN_2x10)
+    #define NS_PLATFORM_MAC_CORE(_port)           (((_port) == 0) ? 0 : 1)
+    #define NS_PLATFORM_MAC_CORE_SERDES_LO(_port) 0
+    #define NS_PLATFORM_MAC_CORE_SERDES_HI(_port) 0
+    #define NS_PLATFORM_MAC_CORE_SERDES_TO_PORT(_mac, _core, _serdes) (_core)
+    #define NS_PLATFORM_MAC_CHANNEL_LO(_port)     ((_port == 0) ? 0 : 64)
+    #define NS_PLATFORM_MAC_CHANNEL_HI(_port)   \
+        (NS_PLATFORM_MAC_CHANNEL_LO(_port) + 3)
+    #define NS_PLATFORM_MAC_PCP_REMAP(_pcp)       ((_pcp <= 3) ? _pcp : 3)
+    #define NS_PLATFORM_MAC_UNTAGGED_MAP          3
+    #define NS_PLATFORM_NUM_PORTS_PER_MAC_0       2
+    #define NS_PLATFORM_NUM_PORTS_PER_MAC_1       0
+    #define NS_PLATFORM_PCLK                      800
+    #define NS_PLATFORM_PORT_SPEED(_port)         10
+    #define NS_PLATFORM_TCLK                      800
+    #define NS_PLATFORM_MAC_0_CORE_0_PORTS_MASK   0x001
+    #define NS_PLATFORM_MAC_0_CORE_1_PORTS_MASK   0x001
+    #define NS_PLATFORM_MAC_1_CORE_0_PORTS_MASK   0x000
+    #define NS_PLATFORM_MAC_1_CORE_1_PORTS_MASK   0x000
+
+/* Scarif 2x10GE */
+#elif (NS_PLATFORM_TYPE == NS_PLATFORM_SCARIF)
     #define NS_PLATFORM_MAC_CORE(_port)           (((_port) == 0) ? 0 : 1)
     #define NS_PLATFORM_MAC_CORE_SERDES_LO(_port) 0
     #define NS_PLATFORM_MAC_CORE_SERDES_HI(_port) 0
